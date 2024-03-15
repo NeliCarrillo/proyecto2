6c16ac5387a5b28e68b800b2ee9be497cc56b2e2
titobalza <titobalzaromero@gmail.com>
2024-03-15 18:05:20 -0400

    CORRECCION DE ARCHIVOS NO GUARDADOS EN PREVIO PUSH

--- Proyecto2BalzaCarrillo/src/Hashtable/Hashtable.java
+++ Proyecto2BalzaCarrillo/src/Hashtable/Hashtable.java
@@ -5,9 +5,209 @@
 package Hashtable;
 
 /**
- *
- * @author @tito_
+ * Clase que define el hashtable a utilizar para almacenar los clientes hospedados
+ * @author tito_
  */
 public class Hashtable {
+
+    //Atributos de la clase
+    private Lista[] array;
+    private int hashSize;
+
+    /**
+     * Constructor de la clase
+     * @param hashSize, longitud del hashtable
+     */
+    public Hashtable(int hashSize) {
+        this.array = new Lista[hashSize];
+        this.hashSize = hashSize;
+    }
+
+    /**
+     * Metodo que permite obtener la lista de clientes almacenados en una clave del hashtable
+     * @return array de clientes almacenados en una clave del hashtable
+     */
+    public Lista[] getArray() {
+        return array;
+    }
+
+    /**
+     * Metodo que permite modificar la lista de clientes almacenados en una clave del hashtable
+     * @param array, nueva lista de clientes a almacenar en una clave del hashtable
+     */
+    public void setArray(Lista[] array) {
+        this.array = array;
+    }
+
+    /**
+     * Metodo que permite obtener la longitud del hashtable
+     * @return longitud del hashtable
+     */
+    public int getHashSize() {
+        return hashSize;
+    }
+
+    /**
+     * Metodo que permite modificar la longitud del hashtable
+     * @param hashSize, nueva longitud del hashtable
+     */
+    public void setHashSize(int hashSize) {
+        this.hashSize = hashSize;
+    }
+
+    /**
+     * Metodo que permite obtener el valor ASCII de una palabra
+     * @param palabra 
+     * @return valor ASCII de la palabra ingresada
+     */
+    public int getAsciiValue(String palabra) {
+        int suma = 0;
+
+        for (int i = 0; i < palabra.length(); i++) {
+            char character = palabra.charAt(i);
+            int ascii = (int) character;
+            suma += ascii;
+        }
+        return suma;
+    }
+
+    /**
+     * Metodo que permite obtener el hashcode a partir del valor ASCII del nombre y apellido del cliente
+     * @param name, nombre del cliente
+     * @param lastName, apellido del cliente
+     * @return hashcode del cliente
+     */
+    public int hashCode(String name, String lastName) {
+        int clave;
+        name = name.toLowerCase();
+        lastName = lastName.toLowerCase();
+        String junto = name + lastName;
+        clave = getAsciiValue(junto) % getHashSize();
+        return clave;
+    }
+
+    /**
+     * Metodo que permite insertar un cliente en el hashtable
+     * @param value, cliente a ingresar
+     */
+    public void insertInHashtable(Client value) {
+
+        String name = value.getName();
+        String lastName = value.getLastName();
+        int key = hashCode(name, lastName);
+        Lista subLista = new Lista();
+        subLista.insertFinal(value);
+        Lista valorArreglo = getArray()[key];
+        if (valorArreglo != null) {
+            valorArreglo.insertFinal(value);
+        } else {
+            getArray()[key] = subLista;
+        }
+    }
+
+    /**
+     * Metodo que permite imprimir el hashtable
+     */
+    public void printTable() {
+        for (int i = 0; i < hashSize; i++) {
+            if (array[i] != null) {
+                System.out.println("key: " + i);
+                array[i].printList();
+            }
+
+        }
+    }
+
+    /**
+     * Metodo que permite eliminar un cliente del hashtable
+     * @param name, nombre del cliente a eliminar
+     * @param lastName, apellido del cliente
+     */
+    public void removeHospedado(String name, String lastName) {
+        int clave = hashCode(name, lastName);
+        if (array[clave] != null) {
+            for (int i = 0; i < array[clave].getSize(); i++) {
+                Client currentClient = (Client) array[clave].getDato(i).getElement();
+                if (currentClient.getLastName().equals(lastName)) {
+                    if (currentClient.getRoomNum() != -1) {
+                        array[clave].deleteInIndex(i);
+                    } else {
+                        System.out.println("El cliente aun no ha realizado el check-in y por lo tanto no se le ha asignado una habitacion");
+                    }
+                }
+            }
+        } else {
+            System.out.println("No se ha encontrado ningun cliente alojado bajo ese nombre");
+        }
+    }
+
+    /**
+     * Metodo que permite obtener el numero de habitacion de un huesped
+     * @param name, nombre del cliente cuyo numero de habitacion se quiere obtener
+     * @param lastName, apellido del cliente cuyo numero de habitacion se quiere obtener 
+     * @return numero de habitacion del cliente, o -1 si el cliente no se encuentra hospedado en el hotel
+     */
+    public int searchClient(String name, String lastName) {
+        int clave = hashCode(name, lastName);
+        if (array[clave] != null) {
+            for (int i = 0; i < array[clave].getSize(); i++) {
+                Client currentClient = (Client) array[clave].getDato(i).getElement();
+                if (currentClient.getLastName().equals(lastName)) {
+                    if (currentClient.getRoomNum() != -1) {
+                        return currentClient.getRoomNum();
+                    } else {
+                        System.out.println("El cliente aun no ha realizado el check-in y por lo tanto no se le ha asignado una habitacion");
+                    }
+                }
+            }
+        } else {
+            System.out.println("No se ha encontrado ningun cliente alojado bajo ese nombre");
+        }
+        return -1;
+    }
+
+    /**
+     * Metodo que permite chequear si el cliente se encuentra guardado en el hashtable
+     * @param cliente a buscar
+     * @return valor logico de si el cliente se encuentra guardado en el hashtable
+     */
+    public boolean checkClient(Client cliente) {
+        boolean check = false;
+        String name = cliente.getName();
+        String lastName = cliente.getLastName();
+        int key = hashCode(name, lastName);
+        if (array[key] != null) {
+            for (int i = 0; i < array[key].getSize(); i++) {
+                if (array[key].getDato(i).getElement() == cliente) {
+                    check = true;
+                }
+
+            }
+        }return check;
+    }
     
+    /**
+     * Metodo que permite obtener el cliente registrado a partir del nombre y apellido ingresado
+     * @param name, nombre del cliente
+     * @param lastName, apellido del cliente
+     * @return (Client) cliente registrado con el nombre y apellido ingresado, o null si el cliente no se encuentra registrado 
+     */
+    public Client obtenerCliente(String name, String lastName) {
+      int clave = hashCode(name, lastName);
+      if (array[clave] != null) {
+          for (int i = 0; i < array[clave].getSize(); i++) {
+              Client currentClient = (Client) array[clave].getDato(i).getElement();
+              if (currentClient.getLastName().equals(lastName)) {
+                  if (currentClient.getRoomNum() != -1) {
+                      return currentClient;
+                  } else {
+                      System.out.println("El cliente aun no ha realizado el check-in y por lo tanto no se le ha asignado una habitacion");
+                  }
+              }
+          }
+      } else {
+          System.out.println("No se ha encontrado ningun cliente alojado bajo ese nombre");
+      }
+      return null;
+  }
 }
--- Proyecto2BalzaCarrillo/src/Hashtable/Nodo.java
+++ Proyecto2BalzaCarrillo/src/Hashtable/Nodo.java
@@ -5,9 +5,74 @@
 package Hashtable;
 
 /**
- *
- * @author @tito_
+ * Clase que define a los nodos que conforman a una lista dobkemente enlazada
+ * @author tito_;
+ * @param <T>, clase de los elemento de la lista, en este caso Client
  */
-public class Nodo {
+public class Nodo<T> {
+    
+    //Atributos de la clase
+    private T element;
+    private Nodo next;
+    private Nodo previous;
+
+    /**
+     * Constructor de la clase
+     * @param elemento a almacenar en el nodo 
+     */
+    public Nodo(T elemento) {
+        this.element = elemento;
+        this.next = null;
+        this.previous = null;
+    }
+
+    /**
+     * Metodo que permite obtener el elemento almacenado en un nodo
+     * @return elemento almacenado en el nodo
+     */
+    public T getElement() {
+        return element;
+    }
+
+    /**
+     * Metodo que permite modificar el elemento almacenado en el nodo
+     * @param elemento, nuevo elemento a almacenar en el nodo
+     */
+    public void setElement(T elemento) {
+        this.element = elemento;
+    }
+
+    /**
+     * Metodo que permite obtener el nodo siguiente al actual
+     * @return nodo siguiente al actual
+     */
+    public Nodo getNext() {
+        return next;
+    }
+
+    /**
+     * Metodo que permite modificar el nodo siguiente al actual
+     * @param siguiente, nuevo nodo siguiente al actual
+     */
+    public void setNext(Nodo siguiente) {
+        this.next = siguiente;
+    }
+
+    /**
+     * Metodo que permite obtener el nodo previo al actual
+     * @return nodo previo al actual
+     */
+    public Nodo getPrevious() {
+        return previous;
+    }
+
+    /**
+     * Metodo que permite modificar el nodo previo al actual
+     * @param previous, nuevo nodo previo al actual
+     */
+    public void setPrevious(Nodo previous) {
+        this.previous = previous;
+    }
+    
     
 }
