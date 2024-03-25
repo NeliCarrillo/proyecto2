bc2feeea77705f6eef3e476102016cf360a28724
llppc <ncx2005@gmail.com>
2024-03-24 21:08:43 -0400

    ready GUI's Check-in y Check-Out
    :fire::fire::fire:

--- Proyecto2BalzaCarrillo/src/GUI/CheckOut.java
+++ Proyecto2BalzaCarrillo/src/GUI/CheckOut.java
@@ -21,6 +21,8 @@ public class CheckOut extends javax.swing.JFrame {
         setLocationRelativeTo(null);
         setResizable(false);
         this.menu=men;
+        this.name.setText("");
+        this.surname.setText("");
     }
 
     /**
@@ -34,6 +36,12 @@ public class CheckOut extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         Atras = new javax.swing.JButton();
+        surname = new javax.swing.JTextField();
+        name = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        accept = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -48,7 +56,33 @@ public class CheckOut extends javax.swing.JFrame {
             }
         });
         jPanel1.add(Atras, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 280, -1, -1));
+        jPanel1.add(surname, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 150, 160, -1));
+        jPanel1.add(name, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 150, 160, -1));
 
+        jLabel2.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel2.setFont(new java.awt.Font("Helvetica Neue", 1, 14)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("Ingrese Nombre y Apellido del huésped para realizar check-out del hotel.");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 100, -1, -1));
+
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("Apellido:");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 130, -1, -1));
+
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("Nombre:");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 130, -1, -1));
+
+        accept.setFont(new java.awt.Font("Helvetica Neue", 1, 13)); // NOI18N
+        accept.setText("Confirmar Check-Out");
+        accept.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                acceptActionPerformed(evt);
+            }
+        });
+        jPanel1.add(accept, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 200, -1, -1));
+
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/Hotels Business Website in Brown White Modern Elegance Style.png"))); // NOI18N
         jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
@@ -71,6 +105,10 @@ public class CheckOut extends javax.swing.JFrame {
         this.menu.setVisible(true);
         this.dispose();
     }//GEN-LAST:event_AtrasActionPerformed
+
+    private void acceptActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_acceptActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_acceptActionPerformed
 
     /**
      * @param args the command line arguments
@@ -106,7 +144,13 @@ public class CheckOut extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton Atras;
+    private javax.swing.JButton accept;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField name;
+    private javax.swing.JTextField surname;
     // End of variables declaration//GEN-END:variables
 }
